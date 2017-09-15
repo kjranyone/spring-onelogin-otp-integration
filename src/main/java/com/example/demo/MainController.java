6b77dff19515c6e38fec527af95ab7e53eb4c36c
Kojiro Tanaka <kojiro.tanaka@gmo-media.jp>
2017-09-15 23:08:22 +0900

    add some  changes

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -4,42 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class MainController {
 
-    private OneloginAuth auth;
-
     @RequestMapping("/")
     String welcome(Model model) {
-        model.addAttribute("user", "hoge");
         return "index";
-    }
-
-    @RequestMapping("/security")
-    String security(Model model, HttpServletRequest request) {
-        // 認証が必要なページに相当
-        String oneloginSid = request.getSession().getAttribute("oneloginSid").toString();
-        if (oneloginSid == null) {
-            auth.login();
-        } else {
-            auth.login("/java-saml-tookit-jspsample/attrs.jsp");
-        }
-        return "security";
-    }
-
-    private class OneloginAuth{
-        public OneloginAuth() {
-            //initialize
-        }
-
-        private void login(){
-            //check onelogin session
-        }
-
-        private void login(String path) {
-            System.out.println(path);
-        }
     }
 }
